9a6ea344eeeef9257d253d6935889913bab44179
Vivek <vivekraj78965.vr@gmail.com>
2023-02-22 12:34:09 +0530

    createtask service & installed uuid

--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { stringify } from 'querystring';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TaskService {
@@ -9,4 +10,15 @@ export class TaskService {
   getAllTask():Task[]{
    return this.tasks;
   }
+
+  createTask(title: string , desc: string):Task{
+    const task:Task = {
+    id:uuid(),
+    title ,
+    desc ,
+    status: TaskStatus.OPEN
+    }
+    this.tasks.push(task);
+    return task;
+}
 }
