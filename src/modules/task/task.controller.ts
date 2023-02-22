ab030a5d382fd0944f8957099f6335e20b994289
Vivek <vivekraj78965.vr@gmail.com>
2023-02-22 12:50:14 +0530

    created task

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get ,Param } from '@nestjs/common';
+import { Body, Post } from '@nestjs/common/decorators';
 import { Task } from './task.model';
 import { TaskService } from './task.service';
 
@@ -9,5 +10,14 @@ export class TaskController{
     @Get(':ask')
     getAllTask():Task[]{
         return this.taskService.getAllTask();
+    }
+
+    //creating a task
+    @Post()
+    createTask(
+        @Body('title') title: string,
+        @Body('desc') desc:string,  
+    ):Task{
+        return this.taskService.createTask(title , desc);
     }
 }
